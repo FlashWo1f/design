3444177504224f7f9910c2f9a8a7cd477fbd1b00
Libin <1064048947@qq.com>
2020-03-29 23:24:17 +0800

    hooks test

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { memo, useState, useEffect, useRef } from 'react';
 import { Button } from 'antd';
 
-export default function Main() {
+const Child = memo((props: any) => {
+  console.log('child render...', props.data)
+  const [name, setName] = useState(props.data)
   return (
-    <div>我的毕业设计</div>
+    <div>
+      <div>child</div>
+      <div>{name} --- {props.data}</div>
+    </div>
+  );
+})
+export default function Hook() {
+  console.log('Hook render...')
+  const [count, setCount] = useState(0)
+  const [name, setName] = useState('rose')
+
+  const countRef = useRef(0)
+  useEffect(() => {
+    console.log('use effect...', count)
+    const timer = setInterval(() => {
+      console.log('timer...count:', countRef.current)
+      setCount(++countRef.current)
+    }, 1000)
+    return () => clearInterval(timer)
+  }, [])
+
+  return (
+    <div>
+      <div>
+        {count}
+      </div>
+      <button onClick={() => setCount(count + 1)}>update count </button>
+      <button onClick={() => setName('jack')}>update name </button>
+      <Child data={name} />
+    </div>
   )
 }
